ad8f11d081c352557a5e9d1a19489e4554dbc60e
Tyler Farrior <tylerfarrior@gmail.com>
2023-08-29 18:30:30 -0400

    set up questions array that will be used for inquirer

--- index.js
+++ index.js
@@ -3,8 +3,32 @@ const { writeFile } = require("fs/promises");
 const SVG = require("./lib/svg");
 const { Circle, Triangle, Square } = require('./lib/shapes');
 
-const init = () => {
+const questions = [
+    {
+        type: "list", 
+        name: "shape",
+        message: "What shape would you like to create?",
+        choices: ["Circle", "Triangle", "Square"]
+    },
+    {
+        type: "input",
+        name: "shapeColor", 
+        message: "What color would you like your shape to be? (red, green, blue etc..)"
+    },
+    {
+        type: "input", 
+        name: "text",
+        message: "What would you like your SVG to say? (3 or less characters)"
+    },
+    {
+        type: "input",
+        name: "textColor",
+        message: "What color would you like your text to be? (red, green, blue etc..)"
+    }
+]
 
+const init = () => {
+    
 };
 
 init()
