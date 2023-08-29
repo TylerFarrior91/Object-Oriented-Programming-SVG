b66f110d61f1b0850d7af2525b00874423c1793a
Tyler Farrior <tylerfarrior@gmail.com>
2023-08-29 18:26:26 -0400

    added imports and set up init function

---
+++ index.js
@@ -0,0 +1,10 @@
+const inquirer = require("inquirer");
+const { writeFile } = require("fs/promises");
+const SVG = require("./lib/svg");
+const { Circle, Triangle, Square } = require('./lib/shapes');
+
+const init = () => {
+
+};
+
+init()
